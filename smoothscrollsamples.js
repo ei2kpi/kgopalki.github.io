55e86d3167de55694fdd409128e3ea81a8d6b8bd
kgopalki <gopal@claylabs.com>
2022-07-04 13:47:25 +0530

    Add files via upload

--- smoothscrollsamples.js
+++ smoothscrollsamples.js
@@ -1,5 +1,8 @@
-draw_actual = true;
-draw_spring = true;
+let draw_actual = true;
+let draw_spring = true;
+let draw_consta = true;
+let draw_ball_pit = true;
+
 
 
 let scroll_range = 100000;
@@ -18,6 +21,11 @@ let spring_y = last_y;
 let spring_v = 0;
 let spring_k = 1e-3;
 let spring_damp = 1e-1;
+
+let max_a = 0.02; // extend to max_a = const + delta_y * factor
+let ca_speed = 0;
+let ca_y = last_y;
+let ca_a = 0;
 
 function update_canvas_size() {
     context.canvas.width = context.canvas.clientWidth;
@@ -54,15 +62,51 @@ function update_animation() {
     }
 
     if (draw_spring) {
-        spring_v += delta_t * (spring_k * (curr_y - spring_y) - spring_damp * spring_v);
-        // spring_v = Math.min(Math.max(spring_v, -1), 1)
-        dy = spring_v * delta_t;
-        // if ((spring_y < curr_y) == (dy > 0)) {
-        //     curr_y 
-        // }
-        spring_y += spring_v * delta_t;
+        if (delta_t > 200) {
+            spring_y = curr_y;
+            spring_v = 0;
+        } else {
+            spring_v += delta_t * (spring_k * (curr_y - spring_y) - spring_damp * spring_v);
+            // spring_v = Math.min(Math.max(spring_v, -1), 1)
+            dy = spring_v * delta_t;
+            // if ((spring_y < curr_y) == (dy > 0)) {
+            //     curr_y 
+            // }
+            spring_y += spring_v * delta_t;
+        }
         draw_frame(spring_y, "green");
         // console.log(spring_y, spring_v);
+    }
+
+    if (draw_consta) {
+        let delta_y = curr_y - ca_y;
+        if (delta_t > 200) {
+            ca_y = curr_y;
+            ca_speed = 0;
+            ca_a = 0;
+        } else {
+            if (Math.abs(delta_y) < 1e-5) {
+                ca_y = curr_y;
+                ca_speed = 0;
+                ca_a = 0;
+            } else {
+                if ((delta_y > 0 && ca_speed < 0) || (delta_y < 0 && ca_speed>0)) {
+                    ca_speed = 0;
+                }
+                let dist = Math.abs(ca_speed * ca_speed / 2 / max_a); // S = v^2 / 2a
+                if (dist < Math.abs(delta_y)) {
+                    ca_a = (delta_y > 0) ? max_a : (-max_a);
+                } else {
+                    ca_a = -ca_speed*ca_speed / 2 / delta_y;
+                }
+                console.log(curr_y, ca_y, ca_speed, ca_a, dist, delta_y);
+                ca_y += ca_speed * delta_t + 0.5 * ca_a * delta_t * delta_t;
+                if ((ca_y > curr_y) && (delta_y > 0)) {ca_y = curr_y};
+                if ((ca_y < curr_y) && (delta_y < 0)) {ca_y = curr_y};
+                ca_speed += ca_a * delta_t;
+            }
+        }
+        draw_frame(ca_y, "blue");
     }
 
     last_t = curr_t;
@@ -104,6 +148,20 @@ function update_animation_every_frame() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+    const up = new URLSearchParams(window.location.search);
+    if (up.get('actual') == '0') {
+        draw_actual = false;
+    }
+    if (up.get('spring') == '0') {
+        draw_spring = false;
+    }
+    if (up.get('consta') == '0') {
+        draw_consta = false;
+    }
+    if (up.get('ball_pit') == '0') {
+        draw_ball_pit = false;
+    }
+
     update_body_height();
 
     let root = document.getElementById("root");
